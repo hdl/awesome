765cd65b5b22d90d1f0c621456ce4ef0db727f6d
umarcor <unai.martinezcorral@ehu.eus>
2021-08-09 15:21:26 +0200

    constraints: support 'README.md' as an alternative to 'info.yml'

--- constraints.py
+++ constraints.py
@@ -24,3 +24,24 @@ for item in (_constraints / 'board').glob('**/*.yml'):
         wfptr.write(item.read_text())
         wfptr.write('ref: https://github.com/hdl/constraints/tree/main/board/%s%s%s\n' % (_prefix, _name, _suffix))
         wfptr.write('---\n')
+
+for item in (_constraints / 'board').glob('**/*.md'):
+
+    if item.name != 'README.md':
+        _name = item.stem
+        _prefix = '.todo/'
+        _suffix = '.md'
+        print('·', _name)
+    else:
+        _name = item.parent.name
+        _prefix = ''
+        _suffix = ''
+        print('-', _name)
+
+    with item.open("r") as rfptr:
+        with (_content / 'boards' / (_name + '.md')).open('w') as wfptr:
+            for line in [
+            '---',
+            'ref: https://github.com/hdl/constraints/tree/main/board/%s%s%s' % (_prefix, _name, _suffix)
+        ] + rfptr.read().splitlines()[1:]:
+                wfptr.writelines('{}\n'.format(line))
