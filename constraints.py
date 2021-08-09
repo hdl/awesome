2a88e19d87564f3d7145e77f5ebd1b5be4f34d83
umarcor <unai.martinezcorral@ehu.eus>
2021-08-09 15:21:45 +0200

    constraints: run black

--- constraints.py
+++ constraints.py
@@ -1,47 +1,51 @@
 from pathlib import Path
 
 _root = Path(__file__).resolve().parent
-_constraints = _root.parent / 'constraints'
-_content = _root / 'content'
+_constraints = _root.parent / "constraints"
+_content = _root / "content"
 
-(_content / 'boards').mkdir(exist_ok=True)
+(_content / "boards").mkdir(exist_ok=True)
 
-for item in (_constraints / 'board').glob('**/*.yml'):
+for item in (_constraints / "board").glob("**/*.yml"):
 
-    if item.name != 'info.yml':
+    if item.name != "info.yml":
         _name = item.stem
-        _prefix = '.todo/'
-        _suffix = '.yml'
-        print('·', _name)
+        _prefix = ".todo/"
+        _suffix = ".yml"
+        print("·", _name)
     else:
         _name = item.parent.name
-        _prefix = ''
-        _suffix = ''
-        print('-', _name)
+        _prefix = ""
+        _suffix = ""
+        print("-", _name)
 
-    with (_content / 'boards' / (_name + '.md')).open('w') as wfptr:
-        wfptr.write('---\n')
+    with (_content / "boards" / (_name + ".md")).open("w") as wfptr:
+        wfptr.write("---\n")
         wfptr.write(item.read_text())
-        wfptr.write('ref: https://github.com/hdl/constraints/tree/main/board/%s%s%s\n' % (_prefix, _name, _suffix))
-        wfptr.write('---\n')
+        wfptr.write(
+            "ref: https://github.com/hdl/constraints/tree/main/board/%s%s%s\n"
+            % (_prefix, _name, _suffix)
+        )
+        wfptr.write("---\n")
 
-for item in (_constraints / 'board').glob('**/*.md'):
+for item in (_constraints / "board").glob("**/*.md"):
 
-    if item.name != 'README.md':
+    if item.name != "README.md":
         _name = item.stem
-        _prefix = '.todo/'
-        _suffix = '.md'
-        print('·', _name)
+        _prefix = ".todo/"
+        _suffix = ".md"
+        print("·", _name)
     else:
         _name = item.parent.name
-        _prefix = ''
-        _suffix = ''
-        print('-', _name)
+        _prefix = ""
+        _suffix = ""
+        print("-", _name)
 
     with item.open("r") as rfptr:
-        with (_content / 'boards' / (_name + '.md')).open('w') as wfptr:
+        with (_content / "boards" / (_name + ".md")).open("w") as wfptr:
             for line in [
-            '---',
-            'ref: https://github.com/hdl/constraints/tree/main/board/%s%s%s' % (_prefix, _name, _suffix)
-        ] + rfptr.read().splitlines()[1:]:
-                wfptr.writelines('{}\n'.format(line))
+                "---",
+                "ref: https://github.com/hdl/constraints/tree/main/board/%s%s%s"
+                % (_prefix, _name, _suffix),
+            ] + rfptr.read().splitlines()[1:]:
+                wfptr.writelines("{}\n".format(line))
